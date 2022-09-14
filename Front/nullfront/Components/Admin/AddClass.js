079e12e406c641eb82247f832731429af19c99ac
Johan P <Johan@DESKTOP-M4MSCGK>
2022-09-14 14:13:23 +0200

    minor bug fix

--- Front/nullfront/Components/Admin/AddClass.js
+++ Front/nullfront/Components/Admin/AddClass.js
@@ -7,7 +7,7 @@ export default function AddClass() {
     async function HandleCreateStudent(event, ClassName) {
         event.preventDefault()
 
-        const response = await fetch($`https://localhost:7212/api/Admins/${id}/mood/${ClassName}`, {
+        const response = await fetch(`https://localhost:7212/api/Admins/${id}/mood/${ClassName}`, {
             method: "PUT",
             headers: { 'Content-Type': 'application/json' },
         })
@@ -22,76 +22,15 @@ export default function AddClass() {
     }
 
     return (
-       //behold 
-        <div> 
-
-        
-        <form>
-        <label>
-        Add name of class:
-        <input value={ClassName} onChange={event => setClassName(event.target.value)}type="Text"></input>
-        </label>
-        <button type="submit" onClick={HandleCreateStudent}>Add</button>
-    </form>
-
-        //
-        <div className="emoji-container">
-            <div className="emoji">
-                <img
-                    draggable="false"
-                    alt="image"
-                    src="1.png"
-                    height="100"
-                    width="100"
-                    onClick={(e) => HandleSendMood(e, 1)}
-                ></img>
-            </div>
-
-            <div className="emoji">
-                <img
-                    draggable="false"
-                    alt="image"
-                    src="2.png"
-                    height="100"
-                    width="100"
-                    onClick={(e) => HandleSendMood(e, 2)}
-                ></img>
-            </div>
-
-            <div className="emoji">
-                <img
-                    draggable="false"
-                    alt="image"
-                    src="3.png"
-                    height="100"
-                    width="100"
-                    onClick={(e) => HandleSendMood(e, 3)}
-                ></img>
-            </div>
-
-            <div className="emoji">
-                <img
-                    draggable="false"
-                    alt="image"
-                    src="4.png"
-                    height="100"
-                    width="100"
-                    onClick={(e) => HandleSendMood(e, 4)}
-                ></img>
-            </div>
-
-            <div className="emoji">
-                <img
-                    draggable="false"
-                    alt="image"
-                    src="5.png"
-                    height="100"
-                    width="100"
-                    onClick={(e) => HandleSendMood(e, 5)}
-                ></img>
-            </div>
+        <div>
+            <form>
+                <label>
+                    Create class:
+                    <input value={ClassName} onChange={event => setClassName(event.target.value)} type="Text"></input>
+                </label>
+                <button type="submit" onClick={HandleCreateStudent}>Add</button>
+            </form>
         </div>
-        </div>// behold
     )
 
 }
