1b3369ab822ae7a3c7ac8e279e755556c3423f11
Jens Bech-Sørensen <jebeso@gmail.com>
2022-09-14 10:23:15 +0200

    Moved image files and imported them into studentindex

--- Front/nullfront/pages/studentindex.js
+++ Front/nullfront/pages/studentindex.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import React from 'react'
+import Image from "next/image";
+
 
 export default function StudentIndex() {
   return (
@@ -9,9 +12,31 @@ export default function StudentIndex() {
         <meta name="description" content="OUR_DESCRIPTION" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main className={styles.main}>
 
-      Hello from studentindex.js
+      <div className="emoji-container">
+      <div>Header</div>
+        <div className="emoji">
+        <img alt="image" src="1.png" height="100" width="100"></img>
+        </div>
+
+        <div className="emoji">
+        <img alt="image" src="2.png" height="100" width="100"></img>
+        </div>
+
+        <div className="emoji">
+        <img alt="image" src="3.png" height="100" width="100"></img>
+
+        </div>
+        <div className="emoji">
+        <img alt="image" src="4.png" height="100" width="100"></img>
+
+        </div>
+        <div className="emoji">
+        <img alt="image" src="5.png" height="100" width="100"></img>
+        </div>
+      </div>
        
       </main>
 
@@ -20,3 +45,5 @@ export default function StudentIndex() {
     </div>
   )
 }
+
+
