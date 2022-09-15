2534688e360fc342513d9ee74d5896f4ffad8138
Johan P <Johan@DESKTOP-M4MSCGK>
2022-09-15 11:27:54 +0200

    deleted admindashboard, moved content [id] page

--- Front/nullfront/pages/Admin/[id].js
+++ Front/nullfront/pages/Admin/[id].js
@@ -11,6 +11,12 @@ const AdminView = () => {
         <div>
             <p>Admin ID: {id}</p>
             <AddClass id={id} />
+            <div>
+            <h1>Her skal adminen sin oversikt over klasser og elever vises</h1>
+            <h1>slik "home" var i figma</h1>
+
+        </div>
+
         </div>
     )
   }
