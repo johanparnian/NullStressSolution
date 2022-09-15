28af29f426c096055b43e73d672faceba3d9e160
Johan P <Johan@DESKTOP-M4MSCGK>
2022-09-15 13:11:53 +0200

    added show students i klasser

--- Front/nullfront/pages/Admin/Classes/[id].js
+++ Front/nullfront/pages/Admin/Classes/[id].js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import AddStudentToClass from '../../../Components/Create/AddStudentToClass'
+import ShowStudents from '../../../Components/Show/ShowStudents'
 
 
 const ClassView = () => {
@@ -12,9 +13,9 @@ const ClassView = () => {
         <div>
             <p>Klasse ID: {id}</p>
             <h1>Her vises adminen sin oversikt over Studentene i denne klassen</h1>
+            <ShowStudents></ShowStudents>
 
-
-            <h1>Her legger Admin til nye studenter til klassen hen er i</h1>
+            <h1>Her legger Admin til nye studenter i denne klassen</h1>
             <AddStudentToClass id={id} />
         </div>
     )
