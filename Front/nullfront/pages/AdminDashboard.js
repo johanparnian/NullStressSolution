b866fee0f9b9ea315ac4d4eba6a11e00d88c8052
Johan P <Johan@DESKTOP-M4MSCGK>
2022-09-15 11:24:22 +0200

    minor fix

--- Front/nullfront/pages/AdminDashboard.js
+++ Front/nullfront/pages/AdminDashboard.js
@@ -2,8 +2,9 @@
 export default function AdminDashboard() {
     return (
         <div>
-            <h1>Her skal adminen sin oversikt over klasser og elever</h1>
+            <h1>Her skal adminen sin oversikt over klasser og elever vises</h1>
             <h1>slik "home" var i figma</h1>
+
         </div>
     )
 }
