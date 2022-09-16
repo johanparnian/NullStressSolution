60894c8fc97f05cde62ae14012c8c1cedbb45cfa
Emiliewol <75267954+Emiliewol@users.noreply.github.com>
2022-09-16 09:54:23 +0200

    Testcommit2

--- Front/nullfront/pages/Kontrollpanel.js
+++ Front/nullfront/pages/Kontrollpanel.js
@@ -1,14 +1,7 @@
 import Head from 'next/head'
-import AddStudent from '../Components/CreateStudent'
-import Main from '../Components/Main'
-import CreateAdmin from '../Components/Create/createadmin'
-import ShowStudentsHomeScreen from '../Components/Show/ShowStudentsHomeScreen'
 import ShowInfoAboutAdminProfile from '../Components/Show/ShowInfoAboutAdminProfile'
 import styles from '../styles/Home.module.css'
-import AddStudentToClass from '../Components/Create/AddStudentToClass'
-import Link from 'next/link'
-import AddClass from '../Components/Create/AddClass'
-import ViewAdmins from '../Components/Show/ShowAdmins'
+
 
 import Header from '../Components/Header'
 
