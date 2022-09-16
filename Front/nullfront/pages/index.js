222f9b9b1fcdaac5f33482bd6ef40ed13ffdc137
SolveigSL <solveig_stokka@hotmail.com>
2022-09-16 10:04:18 +0200

    Fixed multiple create admins

--- Front/nullfront/pages/index.js
+++ Front/nullfront/pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import CreateAdmin from '../Components/Create/createadmin'
 import styles from '../styles/Home.module.css'
 import CreateAdmin from '../Components/Create/createadmin'
 
