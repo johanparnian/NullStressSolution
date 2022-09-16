0af6b139ee5243ad9277b50a1ea0bd24ff94e138
SolveigSL <solveig_stokka@hotmail.com>
2022-09-16 11:07:49 +0200

    New component

---
+++ Front/nullfront/Components/Show/ShowStudentStatistics.js
@@ -0,0 +1,18 @@
+import * as ss from 'simple-statistics'
+import {min} from 'simple-statistics'
+
+export default function ShowStudetStatistics(){
+
+    // var ss = require('simple-statistics')
+
+async function getStatistics(){
+    
+}
+
+   
+
+
+
+
+
+}
