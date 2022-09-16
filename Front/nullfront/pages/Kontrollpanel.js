f9fe3b1ed1d6f041aa9f85ad59431ed70c419b6f
Jens Bech-Sørensen <jebeso@gmail.com>
2022-09-16 16:09:37 +0200

    Added method to create student && send sms

--- Front/nullfront/pages/Kontrollpanel.js
+++ Front/nullfront/pages/Kontrollpanel.js
@@ -3,10 +3,7 @@ import ShowInfoAboutAdminProfile from '../Components/Show/ShowInfoAboutAdminProf
 import styles from '../styles/Home.module.css'
 import Header from '../Components/Header'
 import DeleteEditStudentList from '../Components/Admin/DeleteStudentList'
-import CreateStudent from '../Components/Create/CreateStudent'
-import SendSMS from '../Components/Create/SendSMS'
-
-
+import CreateStudentAndSendSMS from '../Components/Create/CreateStudentAndSendSMS'
 
 
 export default function Kontrollpanel() {
@@ -27,9 +24,8 @@ export default function Kontrollpanel() {
             <div id="item-ctrl2"><ShowInfoAboutAdminProfile /></div>
             <div id="item-ctrl3"><DeleteEditStudentList/></div>
             <div id="item-ctrl4"></div>
-            <div id="item-ctrl5"><CreateStudent /></div>
+            <div id="item-ctrl5"><CreateStudentAndSendSMS /></div>
             <div id="item-ctrl6"></div>
-            <SendSMS />
         </div>
 
         
