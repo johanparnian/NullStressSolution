08803561d1b39ddc599a09b0ceafb122a654930f
Johan P <Johan@DESKTOP-M4MSCGK>
2022-09-19 16:34:01 +0200

    vetke

--- Front/nullfront/pages/index.js
+++ Front/nullfront/pages/index.js
@@ -1,36 +1,17 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Header from '../Components/Show/Header'
-import ShowAdmins from '../Components/Show/ShowAdmins'
-import CreateAdmin from '../Components/Create/createadmin'
-
+import ShowStudents from '../Components/Show/ShowStudents'
+import ShowAllStudents from '../Components/Show/ShowAllStudentst'
 
 
 //import ShowClasses from '../Components/Show/ShowClasses'
 
 export default function Index() {
   return (
-    <div className={styles.container}>
-
-
-      <Head>
-        <title>HJEM.</title>
-        <meta name="description" content="OUR_DESCRIPTION" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-
-        <Header />        
-        <ShowAdmins></ShowAdmins>
-        <CreateAdmin></CreateAdmin>
-
-
-
-        {/* <ShowStudents /> */}
-        {/* <CreateAdmin /> */}
-      </main>
-      <footer>Hei fra footer</footer>
-    </div>
+    <>
+    <Header></Header>
+<ShowAllStudents></ShowAllStudents>  
+    </>
   )
 }
