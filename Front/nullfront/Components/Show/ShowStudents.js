20cde07f85b8f72b204e59278a27c706a1b3bb90
Emiliewol <75267954+Emiliewol@users.noreply.github.com>
2022-09-19 13:59:02 +0200

    Lagt inn ShowMood, for hver enkelt student.

--- Front/nullfront/Components/Show/ShowStudents.js
+++ Front/nullfront/Components/Show/ShowStudents.js
@@ -3,31 +3,6 @@ import React from 'react'
 
 
 export default function ShowStudents(props) {
-    //     const getStudents = async () => {
-
-    //         const endpoint = `https://localhost:7212/api/Students
-    // `
-
-    //         const response = await fetch(endpoint).catch(error => {
-    //             console.log(error)
-    //             throw error
-    //         })
-
-    //         if (response.ok) {
-    //             return await response.json()
-    //         }
-
-    //         else {
-    //             console.log(response)
-    //         }
-    //     }
-
-    //     const [students, setStudents] = useState([])
-
-    //      useEffect(() => {
-    //          getStudents().then(students => setStudents(students))
-    //      }, [])
-
     console.log(props.students)
 
     if (!props.students) {
