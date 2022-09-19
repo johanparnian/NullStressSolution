b9f34951d1acb9e6100318d18a77bee3ce9ae6a7
Johan P <Johan@DESKTOP-M4MSCGK>
2022-09-19 10:00:11 +0200

    MandagsCommit

--- Front/nullfront/Components/Show/ShowSchoolClasses.js
+++ Front/nullfront/Components/Show/ShowSchoolClasses.js
@@ -1,41 +1,48 @@
 import { useEffect, useState } from "react";
 import React from 'react'
 
-export default function ShowSchoolClasses() {
-    const getClasses = async () => {
+export default function ShowSchoolClasses(props) {
+    // const getClasses = async () => {
 
-        const endpoint = `https://localhost:7212/api/schoolclasses`
+    //     const endpoint = `https://localhost:7212/api/admin/1`
 
-        const response = await fetch(endpoint).catch(error => {
-            console.log(error)
-            throw error
-        })
+    //     const response = await fetch(endpoint).catch(error => {
+    //         console.log(error)
+    //         throw error
+    //     })
 
-        if (response.ok) {
-            return await response.json()
-        }
+    //     if (response.ok) {
+    //         return await response.json()
+    //     }
 
-        else {
-            console.log(response)
-        }
+    //     else {
+    //         console.log(response)
+    //     }
+    // }
+
+    // const [schoolClasses, setSchoolClasses] = useState([])
+
+    // useEffect(() => {
+    //     getClasses()
+    //         .then(classes => setSchoolClasses(classes))
+    // }, [])
+
+    console.log(props.classes)
+
+    if (!props.classes) {
+        return <></>
     }
-
-    const [schoolClasses, setSchoolClasses] = useState([])
-
-    useEffect(() => {
-        getClasses()
-            .then(classes => setSchoolClasses(classes))
-    }, [])
 
     return (
         <div>
             List of classes:
-            <div key={schoolClasses.id}>
-                {schoolClasses.map((classes) => (
-                    <a href={`Classes/${classes.id}`}><li key={classes.id}>{classes.schoolClassName}</li></a>
+            
+                {props.classes.map((clazz) => (
+                    <div key={clazz.id}>
+                    <a href={`Classes/${clazz.id}`}><li key={clazz.id}>{clazz.schoolClassName}</li></a>
                     
-                    ))}
             </div>
+                    ))}
         </div>
     )
 }
--- Front/nullfront/pages/Admin/[id].js
+++ Front/nullfront/pages/Admin/[id].js
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
 import AddClass from '../../Components/Create/AddClass'
 
 import ShowSchoolClasses from '../../Components/Show/ShowSchoolClasses'
@@ -8,13 +9,25 @@ const AdminView = () => {
     const router = useRouter()
     const { id } = router.query
 
+    const [admin, setAdmin] = useState({})
 
+    useEffect(() => {
+        const endpoint = `https://localhost:7212/api/admins/${id}`
+
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(data => setAdmin(data))
+        .catch(error => {
+            console.log(error)
+            throw error
+        })
+    }, [id])
 
     return (
         <div>
             <p>Admin ID: {id}</p>
             <h1>Adminen sin oversikt over klasser</h1>
-            <ShowSchoolClasses></ShowSchoolClasses>
+            <ShowSchoolClasses classes={admin.schoolClasses}></ShowSchoolClasses>
             <h1>Her legger Admin til nye klasser</h1>
             <AddClass id={id} />
         </div>
