62ad97c4ce615e72612ecff560af1bfc73561f41
Jens Bech-Sørensen <jebeso@gmail.com>
2022-09-20 10:19:17 +0200

    Added footer and imporved design

--- Front/nullfront/pages/AdminOverview.js
+++ Front/nullfront/pages/AdminOverview.js
@@ -1,34 +1,32 @@
 import ShowAdmins from "../Components/Show/ShowAdmins";
 import CreateAdmin from "../Components/Create/createadmin";
 import Header from "../Components/Show/Header";
+import Footer from "../Components/Show/Footer";
 
 import ShowInfoAboutAdminProfile from "../Components/Show/ShowInfoAboutAdminProfile";
 
 export default function AdminOverview() {
     return (
-        <div>
+        <>
             <Header></Header>
             <div className="container">
-                    <div className="row" id="mainview">
-                        <div className="col-1" id="white"></div>
-                        <div className="col"><ShowAdmins></ShowAdmins></div>
-                        <div className="col-5"><CreateAdmin></CreateAdmin></div>
-                        <div className="col-1" id="white"></div>
-                    </div>
+                <div className="row" id="mainview">
+                    <div className="col-1" id="white"></div>
 
-                    <div class="row">
-                        <div className="col-1" id="white"></div>
-                        <div class="col"></div>
-                        <div className="col"></div>
-                        <div className="col-1" id="white"></div>
+                    <div className="overskrift1">
+                        Velg eller legg til ny lærer
                     </div>
-
-                    <div className="row">
-                        <div className="col-1" id="white"><h2></h2></div>
-                        <div className="col"><h2>Footer</h2></div>
-                        <div className="col-1" id="white"><h2></h2></div>
-                    </div>
+                    <div className="col-1" id="white"></div>
                 </div>
-        </div>
+
+                <div className="row" id="mainview">
+                    <div className="col-1" id="white"></div>
+                    <div className="col"><ShowAdmins></ShowAdmins></div>
+                    <div className="col"><CreateAdmin></CreateAdmin></div>
+                    <div className="col-1" id="white"></div>
+                </div>
+            <Footer></Footer>
+            </div>
+        </>
     )
 }
