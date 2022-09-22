b542aa7ca32dbf437d00f3398593d4c224218182
Johan P <Johan@DESKTOP-M4MSCGK>
2022-09-22 11:42:15 +0200

    reinstated homemodule.css

--- Front/Statistikk/barchart/pages/index.js
+++ Front/Statistikk/barchart/pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import BarChar from '../Components/Barchar'
+import '../styles/Home.module.css'
+
 
 export default function Home() {
   return (
